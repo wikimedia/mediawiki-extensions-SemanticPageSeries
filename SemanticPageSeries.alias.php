272d569d13323ee884e63c47a7e5da307b40ea92
Siebrand Mazeland <siebrand@users.mediawiki.org>
2012-05-23 19:28:24 +0000

    Localisation updates from http://translatewiki.net.

--- SemanticPageSeries.alias.php
+++ SemanticPageSeries.alias.php
@@ -7,12 +7,35 @@
  * @file
  * @ingroup SemanticPageSeries
  */
- 
+
 $specialPageAliases = array();
- 
-/** English
- */
+
+/** English (English) */
 $specialPageAliases['en'] = array(
-        'SeriesEdit' => array( 'SeriesEdit' ),
+	'SeriesEdit' => array( 'SeriesEdit' ),
 );
- 
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'SeriesEdit' => array( 'Seitenabfolge_bearbeiten' ),
+);
+
+/** Swiss German (Alemannisch) */
+$specialPageAliases['gsw'] = array(
+	'SeriesEdit' => array( 'Syteabfolg_bearbeite' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'SeriesEdit' => array( 'УредувањеНаНизи' ),
+);
+
+/** Norwegian Bokmål (‪Norsk (bokmål)‬) */
+$specialPageAliases['nb'] = array(
+	'SeriesEdit' => array( 'Serieredigering' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'SeriesEdit' => array( 'SeriesBewerken' ),
+);
