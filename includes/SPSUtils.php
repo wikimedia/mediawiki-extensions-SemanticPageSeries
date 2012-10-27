029384b08e3f12bc15bbe85631bbb61c6a2c0ed8
Stephan Gambke <s7eph4n@gmail.com>
2012-10-27 21:44:38 +0200

    bugfix (page create fails with error: "The start date is missing.")
    Change-Id: I421631a30d47a1476eb717a590c8849645409956

--- includes/SPSUtils.php
+++ includes/SPSUtils.php
@@ -2,7 +2,7 @@
 
 /**
  * File holding the SPSUtils class
- * 
+ *
  * @author Stephan Gambke
  * @file
  * @ingroup SemanticPageSeries
@@ -20,11 +20,11 @@ class SPSUtils {
 
 	/**
 	 * Initialize the parser functions of the extension.
-	 * 
+	 *
 	 * Currently only #serieslink
-	 * 
+	 *
 	 * @param Parser $parser
-	 * @return bool 
+	 * @return bool
 	 */
 	static public function initParserFunction( &$parser ) {
 
@@ -38,15 +38,15 @@ class SPSUtils {
 
 	/**
 	 * Renders the #serieslink parser function.
-	 * 
+	 *
 	 * @param Parser $parser
-	 * @return string the unique tag which must be inserted into the stripped text 
+	 * @return string the unique tag which must be inserted into the stripped text
 	 */
 	static public function renderSeriesLink( &$parser ) {
-		
+
 		$params = func_get_args();
 		array_shift( $params ); // We don't need the parser.
-		
+
 		// remove the target parameter should it be present
 		foreach ( $params as $key => $value ) {
 			$elements = explode( '=', $value, 2 );
@@ -61,25 +61,25 @@ class SPSUtils {
 		// set the origin parameter
 		// This will block it from use as iterator parameter. Oh well.
 		$params[] = "origin=" . $parser->getTitle()->getArticleID();
-		
+
 		// hack to remove newline from beginning of output, thanks to
 		// http://jimbojw.com/wiki/index.php?title=Raw_HTML_Output_from_a_MediaWiki_Parser_Function
 		return $parser->insertStripItem( SFUtils::createFormLink( $parser, 'SeriesEdit', $params ), $parser->mStripState );
 	}
-	
-	
+
+
 	/**
 	 * Returns a value from an array if the $key exists, else returns the default.
-	 * 
+	 *
 	 * The value is trimmed.
-	 * 
+	 *
 	 * @param array $array
 	 * @param mixed $key
 	 * @param mixed $default
-	 * @return mixed 
+	 * @return mixed
 	 */
 	static public function fromArray( &$array, $key, $default = null ) {
-		if ( array_key_exists( $key, $array ) ) {
+		if ( array_key_exists( $key, $array ) && is_string( $array[$key] ) ) {
 			return trim( $array[$key] );
 		} else {
 			return $default;
@@ -95,9 +95,9 @@ class SPSUtils {
 
 		$args = func_get_args();
 		array_shift($args);
-		
+
 		$msg= new Message($key,$args);
-		
+
 		return $msg->inContentLanguage()->parse();
 	}
 }
--- includes/iterators/SPSDateIterator.php
+++ includes/iterators/SPSDateIterator.php
@@ -2,7 +2,7 @@
 
 /**
  * File holding the SPSDateIterator class
- * 
+ *
  * @author Stephan Gambke
  * @file
  * @ingroup SemanticPageSeries
@@ -17,7 +17,7 @@ if ( !defined( 'SPS_VERSION' ) ) {
  * @ingroup SemanticPageSeries
  */
 class SPSDateIterator extends SPSIterator {
-	
+
 	/**
 	 * @return array An array containing the names of the parameters this iterator uses.
 	 */
@@ -29,21 +29,59 @@ class SPSDateIterator extends SPSIterator {
 			'unit' => SPS_OPTIONAL
 			);
 	}
-	
+
 	/**
 	 * @return an array of the values to be used in the target field of the target form
 	 */
 	function getValues ( &$data ){
-		
-		$start  = SPSUtils::fromArray( $data, 'start' );
-		$end    = SPSUtils::fromArray( $data, 'end', $start );
+
+		if ( array_key_exists( 'start', $data ) ) {
+
+			if ( is_string( $data['start'] ) ) {
+
+				// start date provided as a string (e.g. by datepicker input)
+				$start = trim( $data['start'] );
+
+			} else if ( is_array( $data['start']) &&
+				array_key_exists( 'day', $data['start'] ) &&
+				array_key_exists( 'month', $data['start'] ) &&
+				array_key_exists( 'year', $data['start'] ) ) {
+
+				// start date provided as an array (e.g. by normal date input)
+				$start = trim( $data['start']['year'] ) . '/' .
+					trim( $data['start']['month'] ) . '/' .
+					trim( $data['start']['day'] ) ;
+
+			} else {
+				throw new SPSException( SPSUtils::buildMessage( 'spserror-date-startdatemissing' ) );
+			}
+		}
+
+		if ( array_key_exists( 'end', $data ) ) {
+
+			if ( is_string( $data['end'] ) ) {
+
+				// end date provided as a string (e.g. by datepicker input)
+				$end = trim( $data['end'] );
+				
+			} else if ( is_array( $data['end']) &&
+				array_key_exists( 'day', $data['end'] ) &&
+				array_key_exists( 'month', $data['end'] ) &&
+				array_key_exists( 'year', $data['end'] ) ) {
+
+				// start date provided as an array (e.g. by normal date input)
+				$end = trim( $data['end']['year'] ) . '/' .
+					trim( $data['end']['month'] ) . '/' .
+					trim( $data['end']['day'] );
+
+			} else {
+				$end = $start;
+			}
+		}
+
 		$period = SPSUtils::fromArray( $data, 'period', 1 );
 		$unit   = SPSUtils::fromArray( $data, 'unit', 'day' );
-		
-		if ( $start === null || $start === '' ) {
-			throw new SPSException( SPSUtils::buildMessage( 'spserror-date-startdatemissing' ) );
-		}
-		
+
 		//prepare params for getDatesForRecurringEvent
 		$params = array (
 			'property=SomeDummyProperty',
@@ -54,7 +92,7 @@ class SPSDateIterator extends SPSIterator {
 			);
 
 		$values = SMWSetRecurringEvent::getDatesForRecurringEvent($params);
-		
+
 		if ( $values === null ) {
 			throw new SPSException( SPSUtils::buildMessage( 'spserror-date-internalerror' ) );
 		}
